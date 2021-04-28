b1e2cf4420f5e5193583502ae5e39c8a6428f10b
SourceCode347 <80837445+sourcecode347@users.noreply.github.com>
2021-04-28 12:41:16 +0300

    Add files via upload

--- spider.py
+++ spider.py
@@ -88,7 +88,11 @@ def is_vulnerable(source):
     return False
 openbrowser()
 while True:
-   dork = "https://duckduckgo.com/?q="+r.get_random_word()+"+%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
+   rnum = random.randint(0,3)
+   if rnum == 0:
+       dork = "https://duckduckgo.com/?q="+r.get_random_word()+"+%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
+   else:
+       dork = "https://duckduckgo.com/?q="+r.get_random_word()+"+inurl:%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
    navigate(dork)
    testlinks=[]
    for x in range(0,5):
