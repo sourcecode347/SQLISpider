e0567cd83ae7bf758d403982e9cfa8f2dce0c4cf
SourceCode347 <80837445+sourcecode347@users.noreply.github.com>
2021-04-28 15:34:00 +0300

    Add files via upload

--- spider.py
+++ spider.py
@@ -90,11 +90,11 @@ openbrowser()
 while True:
    rnum = random.randint(0,3)
    if rnum ==0:
-       dork = "https://duckduckgo.com/?q=php%3Fid%3D"+str(random.randint(0,100000))+"+"+r.get_random_word()+"&t=h_&ia=web"
+       dork = "https://duckduckgo.com/?q=php%3Fid%3D"+str(random.randint(0,100000))+"+"+str(r.get_random_word())+"&t=h_&ia=web"
    elif rnum ==1:
        dork = "https://duckduckgo.com/?q=inurl:%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
    else:
-       dork = "https://duckduckgo.com/?q="+r.get_random_word()+"+inurl:%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
+       dork = "https://duckduckgo.com/?q="+str(r.get_random_word())+"+inurl:%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
    navigate(dork)
    testlinks=[]
    for x in range(0,5):
